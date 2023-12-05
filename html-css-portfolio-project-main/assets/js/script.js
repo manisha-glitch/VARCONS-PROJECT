45bd7f9970a6f6ea7d283a77f7a197f08d7ba0b0
manisha-glitch <114828048+manisha-glitch@users.noreply.github.com>
2023-12-05 13:22:53 +0530

    Delete html-css-portfolio-project-main directory

--- html-css-portfolio-project-main/assets/js/script.js
+++
@@ -1,7 +0,0 @@
-// This code snippet was from FlorinPop's YouTube tutorial 
-const hamburger = document.getElementById("hamburger");
-const navbarUL = document.getElementById("navbar-ul");
-
-hamburger.addEventListener("click", () => {
-    navbarUL.classList.toggle("show");
-});
